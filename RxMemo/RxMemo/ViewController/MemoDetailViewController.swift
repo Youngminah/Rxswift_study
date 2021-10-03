aa8623177d9db3ebee5411c994c036ba39ee1c5c
Youngminah <bluebelsmudi@gmail.com>
2021-10-03 15:12:14 +0900

    Add: ViewController Class

---
+++ RxMemo/RxMemo/ViewController/MemoDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MemoDetailViewController.swift
+//  RxMemo
+//
+//  Created by meng on 2021/10/03.
+//
+
+import UIKit
+
+class MemoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ RxMemo/RxMemo/ViewController/MemoListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MemoListViewController.swift
+//  RxMemo
+//
+//  Created by meng on 2021/10/03.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
