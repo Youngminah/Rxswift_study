5dd5dad5dcd9278412a9da30e7477bd36a2afb00
Youngminah <bluebelsmudi@gmail.com>
2021-10-08 23:13:57 +0900

    Feat: Custom extension

--- RxMemo/RxMemo/ViewController/MemoComposeViewController.swift
+++ RxMemo/RxMemo/ViewController/MemoComposeViewController.swift
@@ -58,19 +58,28 @@ class MemoComposeViewController: UIViewController, ViewModelBindableType {
             .map { noti -> CGFloat in 0 }
         
         let keyboardObservable = Observable.merge(willShowObservable, willHideObservable).share()
-        keyboardObservable.subscribe(onNext: { [weak self] height in
-            guard let strongSelf = self else { return }
-            var inset = strongSelf.contentTextView.contentInset
+        
+        keyboardObservable
+            .toContentInset(of: contentTextView)
+            .bind(to: contentTextView.rx.contentInset)
+            .disposed(by: rx.disposeBag)
+    }
+}
+
+extension ObservableType where Element == CGFloat {
+    func toContentInset(of textView: UITextView) -> Observable<UIEdgeInsets> {
+        return map { height in
+            var inset = textView.contentInset
             inset.bottom = height
-            
-            var scrollInset = strongSelf.contentTextView.scrollIndicatorInsets
-            scrollInset.bottom = height
-            
-            UIView.animate(withDuration: 0.3) {
-                strongSelf.contentTextView.contentInset = inset
-                strongSelf.contentTextView.scrollIndicatorInsets = scrollInset
-            }
-        })
-        .disposed(by: rx.disposeBag)
+            return inset
+        }
+    }
+}
+
+extension Reactive where Base: UITextView {
+    var contentInset: Binder<UIEdgeInsets> {
+        return Binder(self.base) { textView, inset in
+            textView.contentInset = inset
+        }
     }
 }
